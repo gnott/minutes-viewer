343951e48a20371217c07fae1b4e94312cafad49
Graham Nott <gnott@starglobal.ca>
2013-02-23 17:21:59 -0800

    Initial commit.

---
+++ data/data.js
@@ -0,0 +1,129 @@
+var varianceCollection =
+    { "type": "FeatureCollection",
+        "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "category": "default",
+                "title": "Development Permit with Variances Application",
+                "id": "000247",
+                "address": "950 Wharf Street",
+                "date": "2013-02-07",
+                "url": "https://victoria.civicweb.net/Documents/DocumentList.aspx?ID=46517"
+            },
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [-123.3697267, 48.4243759]
+            }
+        }
+        ,{
+            "type": "Feature",
+            "properties": {
+                "category": "default",
+                "title": "Heritage Designation Application",
+                "id": "000124",
+                "address": "1250 Denman Street",
+                "date": "2013-01-10",
+                "url": "https://victoria.civicweb.net/Documents/DocumentList.aspx?ID=46399"
+            },
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [-123.34724670000003, 48.4328727]
+            }
+        }
+        ,{
+            "type": "Feature",
+            "properties": {
+                "category": "default",
+                "title": "Heritage Designation Application",
+                "id": "000125",
+                "address": "614 Niagara Street",
+                "date": "2013-01-10",
+                "url": "https://victoria.civicweb.net/Documents/DocumentList.aspx?ID=46399"
+            },
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [-123.37146849999999, 48.4118995]
+            }
+        } 
+        ,{
+            "type": "Feature",
+            "properties": {
+                "category": "default",
+                "title": "Rezoning Application",
+                "id": "00358",
+                "address": "151 Oswego Street",
+                "date": "2013-01-10",
+                "url": "https://victoria.civicweb.net/Documents/DocumentList.aspx?ID=46399"
+            },
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [-123.3791114, 48.4169372]
+            }
+        }  
+        ,{
+            "type": "Feature",
+            "properties": {
+                "category": "default",
+                "title": "Development Permit Application",
+                "id": "000291",
+                "address": "257 Belleville Street",
+                "date": "2013-01-10",
+                "url": "https://victoria.civicweb.net/Documents/DocumentList.aspx?ID=46399"
+            },
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [-123.37593170000002, 48.4220635]
+            }
+        }  
+        ,{
+            "type": "Feature",
+            "properties": {
+                "category": "default",
+                "title": "Heritage Alteration Permit Application",
+                "id": "00149",
+                "address": "1107 Wharf Street",
+                "date": "2013-01-10",
+                "url": "https://victoria.civicweb.net/Documents/DocumentList.aspx?ID=46399"
+            },
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [-123.36968289999999, 48.4253869]
+            }
+        }  
+        ,{
+            "type": "Feature",
+            "properties": {
+                "category": "default",
+                "title": "Heritage Alteration Permit Application (with Variances)",
+                "id": "00155",
+                "address": "203 Raynor Avenue",
+                "date": "2013-01-10",
+                "url": "https://victoria.civicweb.net/Documents/DocumentList.aspx?ID=46399"
+            },
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [-123.3834913, 48.4365453]
+            }
+        }
+        ,{
+            "type": "Feature",
+            "properties": {
+                "category": "default",
+                "title": "Heritage Designation Application",
+                "id": "000126",
+                "address": "1 Centennial Square",
+                "date": "2013-01-10",
+                "url": "https://victoria.civicweb.net/Documents/DocumentList.aspx?ID=46399"
+            },
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [-123.36475689999997, 48.428735]
+            }
+        }  
+        
+        
+        ]
+
+    };
+
